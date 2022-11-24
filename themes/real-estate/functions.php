14fc2e167e70630411dfe157ad916af48527bbca
NguyenLouis <louis.nguyen@isphers.fr>
2022-11-24 09:59:43 +0100

    add home.css

--- themes/real-estate/functions.php
+++ themes/real-estate/functions.php
@@ -18,8 +18,9 @@ function real_estate_supports(){
 function real_estate_register_assets() {
     // style file
     wp_enqueue_style( 'style', get_stylesheet_uri() );
+    wp_enqueue_style('home', get_template_directory_uri() . './assets/css/home.css');
     // script file
-    wp_enqueue_script("main", get_template_directory_uri() ."./assets/js/main.js");
+    wp_enqueue_script('main', get_template_directory_uri() .'./assets/js/main.js');
 
     //boostrap assets
         wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css');
